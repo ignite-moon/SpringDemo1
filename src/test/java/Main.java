34e885a2235c52289c21857bf29f5038956aa1f7
lq <2280258672@qq.com>
2022-04-13 14:04:44 +0800

    second

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -5,7 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import java.util.List;
 
 public class Main {
@@ -19,17 +18,22 @@ public class Main {
     @Test
     public void asd(){
         //ApplicationContext ac=new ClassPathXmlApplicationContext("classpath:spring-service.xml","classpath:spring-dao.xml");
-        //applicationContext.xml为spring-service和spring-dao的结合体，经测试成功
+        //applicationContext.xml为spring-service、spring-dao、spring-mvc的结合体，经测试成功
         ApplicationContext ac=new ClassPathXmlApplicationContext("applicationContext.xml");
         UserService userService=(UserService) ac.getBean("userService");
 
-        /*User user=new User();
-        user.setId(7);
-        user.setName("sss");
-        user.setPassword("777");
-        userService.insert(user);*/
-
-        //userService.deleteById(4);
+//        User user=new User();
+//        user.setId(9);
+//        user.setName("sss");
+//        user.setPassword("357");
+//        userService.insert(user);
+//
+//        userService.deleteById(2);
+//
+//        User u=new User();
+//        u.setId(7);
+//        u.setPassword("999");
+//        userService.updateById(u);
 
         List<User> users=userService.selectAll();
         System.out.println(users);
